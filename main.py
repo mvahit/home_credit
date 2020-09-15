98c5dbbfb1322413fc8f28489a1503b0c0a4f705
mvahit <m.vahitkeskin@gmail.com>
2020-09-15 23:49:47 +0200

    commit from make file

--- main.py
+++ main.py
@@ -22,63 +22,54 @@ def timer(title):
 def main(debug=False):
     num_rows = 10000 if debug else None
 
-    with timer("Process application train"):
-        df = application_train_test(num_rows)
-        print("application train & test shape:", df.shape)
+    with timer("Pre-Processing"):
 
-    with timer("Process bureau and bureau_balance"):
+        i_love_ds()
+
+        # application_train_test
+        df = application_train_test(num_rows)
+
+        # bureau & bureau_balance
         bureau = bureau_and_balance(num_rows)
-        print("Bureau df shape:", bureau.shape)
         df = df.join(bureau, how='left', on='SK_ID_CURR')
         del bureau
-        gc.collect()
 
-    with timer("Process previous_applications"):
+        # previous_applications
         prev = previous_applications(num_rows)
-        print("Previous applications df shape:", prev.shape)
         df = df.join(prev, how='left', on='SK_ID_CURR')
         del prev
-        gc.collect()
 
-    with timer("Process POS-CASH balance"):
+        # posh_cash
         pos = pos_cash(num_rows)
-        print("Pos-cash balance df shape:", pos.shape)
         df = df.join(pos, how='left', on='SK_ID_CURR')
         del pos
-        gc.collect()
 
-    with timer("Process installments payments"):
+        # installments_payments
         ins = installments_payments(num_rows)
-        print("Installments payments df shape:", ins.shape)
         df = df.join(ins, how='left', on='SK_ID_CURR')
         del ins
-        gc.collect()
 
-    with timer("Process credit card balance"):
+        # credit_card_balance
         cc = credit_card_balance(num_rows)
-        print("Credit card balance df shape:", cc.shape)
         df = df.join(cc, how='left', on='SK_ID_CURR')
+        del cc
 
         # saving final dataframes
         train_df = df[df['TARGET'].notnull()]
         test_df = df[df['TARGET'].isnull()]
         train_df.to_pickle("data/final_train_df.pkl")
         test_df.to_pickle("data/final_test_df.pkl")
-
-        del cc, train_df, test_df
+        del train_df, test_df
         gc.collect()
 
-    with timer("Run LightGBM with kfold"):
+    with timer("Run LightGBM"):
         feat_importance = kfold_lightgbm(df, debug=debug)
 
 
 if __name__ == "__main__":
     namespace = get_namespace()
-    i_love_ds()
     with timer("Full model run"):
         main(debug=namespace.debug)
-
-
 
 # kaggle model run: 7879s
 # server: 8290s
