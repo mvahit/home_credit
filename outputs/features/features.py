8ee64593a8a9b6440dc077d8411e5093e2fed498
mvahit <m.vahitkeskin@gmail.com>
2020-11-26 18:51:00 +0100

    commit from make file

--- outputs/features/features.py
+++ outputs/features/features.py
@@ -12,3 +12,13 @@ df[df["mean"] > 0]
 
 df2 = pd.read_pickle("/Users/mvahit/Documents/GitHub/home_credit/outputs/features/fold_auc_best_df.pkl")
 
+
+# FINAL DF
+
+import pandas as pd
+pd.set_option('display.max_columns', None)
+df = pd.read_pickle("/Users/mvahit/Documents/GitHub/home_credit/data/final_train_df.pkl")
+df.head()
+
+[col for col in df.columns if col.startswith("APP")]
+a = df[[col for col in df.columns if col.startswith("APP")]].head()
